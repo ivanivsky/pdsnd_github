fd590164dec1158b580d8f3d57ac5dbeb114577a
Paul Ivanivsky <ivanivsky@gmail.com>
2020-09-22 17:16:38 -0400

    style: Specified city names when requesting input
    The city names were missing when asking users to input a city. The choices were unclear.

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
 
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    city = input('\nPlease enter the city: ').lower()
+    city = input('\nPlease enter the city: New York City, Chicago, Washington').lower()
     while city not in ('chicago', 'new york city', 'washington'):
         print('You must enter a valid city')
         city = input('\nPlease enter the city: ').lower()
